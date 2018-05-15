8a87bd364b56b5b0f5183d0902e6a1c55ca0598f
vinmino <vinlomino@gmail.com>
2018-05-15 08:18:08 -0400

    Added FileIO module

---
+++ FileIO/src/replaceText/ReplaceText.java
@@ -0,0 +1,11 @@
+package replaceText;
+
+public class ReplaceText {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+
+}
