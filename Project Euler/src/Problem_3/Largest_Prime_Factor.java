d8dd1a17eb840f9adf64a7d9c82411e6524cd598
Vincent Lomino <vinlomino@gmail.com>
2018-05-16 00:08:09 -0400

    Total fail at a start of Problem_3

--- Project Euler/src/Problem_3/Largest_Prime_Factor.java
+++ Project Euler/src/Problem_3/Largest_Prime_Factor.java
@@ -1,4 +1,14 @@
 package Problem_3;
 
 public class Largest_Prime_Factor {
+
+    public static void main(String[] args) {
+        double num = 600851475143d;
+        for (double i = 2d; i*i <= num; i++) {
+            if (num % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
 }
