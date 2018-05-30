256b58c3c8f59ba64bdd5af081c2df2111ca746b
Vincent Lomino <vinlomino@gmail.com>
2018-05-29 21:36:01 -0400

    Completed the VennDiagram, its not commented but I don't really want to... :)

--- Learning JavaFX/src/VennDiagram.java
+++ Learning JavaFX/src/VennDiagram.java
@@ -5,8 +5,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-
 public class VennDiagram extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -14,7 +12,7 @@ public class VennDiagram extends Application {
         double WIDTH = 500;
         double xCenter = WIDTH / 2;
         double yCenter = HEIGHT / 2;
-        int numOfCircles = 50;
+        int numOfCircles = 3;
         double tempX;
         double tempY;
         double circleRadii = 100;
@@ -26,11 +24,10 @@ public class VennDiagram extends Application {
             tempX = xCenter + (circleRadii / 2) * Math.cos(angle * i);
             tempY = yCenter + (circleRadii / 2) * Math.sin(angle * i);
 
-
             circleArray[i] = new Circle(tempX, tempY, circleRadii);
-            circleArray[i].setFill(null);
+            circleArray[i].setFill((i == 1) ? new Color(1, 0, 0, 0.5) : (i == 0) ? new Color(0, 1, 0, 0.5) : new Color(0, 0, 1, 0.5));
             circleArray[i].setStroke(Color.BLACK);
-            circleArray[i].setStrokeWidth(2);
+            circleArray[i].setStrokeWidth(1);
             pane.getChildren().add(circleArray[i]);
         }
         Scene scene = new Scene(pane, WIDTH, HEIGHT);
