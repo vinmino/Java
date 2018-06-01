3eba8234289f1f833bfef6b948c5afae4f7da1b3
vinmino <vinlomino@gmail.com>
2018-06-01 14:07:34 -0400

    I did absolutely nothing, the TranslationTranstion did not work, I will try to use a Timeline or PathTransition in the future

--- Learning JavaFX/src/Day_3/PieceOfThePi.java
+++ Learning JavaFX/src/Day_3/PieceOfThePi.java
@@ -1,5 +1,6 @@
 package Day_3;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -8,9 +9,9 @@ import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
-import javax.swing.*;
-import java.util.Random;
+//https://examples.javacodegeeks.com/desktop-java/javafx/javafx-animation-example/
 
 public class PieceOfThePi extends Application {
     @Override
@@ -24,21 +25,21 @@ public class PieceOfThePi extends Application {
         testingCircle.setStrokeWidth(1);
         testingCircle.setStroke(Color.TRANSPARENT);
         Arc[] piePieces = new Arc[NumOfPieces];
-        for (int i = 0; i < piePieces.length - 1; i++) {
+        for (int i = 0; i < piePieces.length; i++) {
             piePieces[i] = new Arc();
             piePieces[i].setCenterX(WIDTH / 2);
             piePieces[i].setCenterY(HEIGHT / 2);
             piePieces[i].setRadiusX(RADIUS);
             piePieces[i].setRadiusY(RADIUS);
             piePieces[i].setType(ArcType.ROUND);
-            piePieces[i].setLength( 360 / NumOfPieces);
+            piePieces[i].setLength(360 / NumOfPieces);
             piePieces[i].setStartAngle(360 * i / NumOfPieces);
             piePieces[i].setStroke(Color.BLACK);
             piePieces[i].setStrokeWidth(1);
             piePieces[i].setFill(Color.hsb(360 * i / NumOfPieces, 1.0, 1.0));
             pane.getChildren().add(piePieces[i]);
         }
-        Arc piece = new Arc();
+        /*Arc piece = new Arc();
         piece.setCenterX((WIDTH / 2) + 20 * Math.cos(2 * Math.PI / (2 * NumOfPieces)));
         piece.setCenterY((HEIGHT / 2) + 20 * Math.sin(2 * Math.PI / (2 * NumOfPieces)));
         piece.setRadiusY(RADIUS);
@@ -49,13 +50,28 @@ public class PieceOfThePi extends Application {
         piece.setStrokeWidth(1);
         piece.setStroke(Color.BLACK);
         piece.setFill(Color.hsb(360 * (piePieces.length - 1) / NumOfPieces, 1.0, 1.0));
-        pane.getChildren().add(piece);
+        pane.getChildren().add(piece);*/
 
         pane.getChildren().add(testingCircle);
         Scene scene = new Scene(pane, WIDTH, HEIGHT);
         stage.setTitle("Vincent Lomino");
         stage.setScene(scene);
         stage.show();
+
+        TranslateTransition transition = new TranslateTransition(Duration.millis(3000));
+
+        for (int i = 0; i < piePieces.length; i++) {
+            transition.setNode(piePieces[i]);
+            transition.setFromX(WIDTH / 2);
+            transition.setFromY(HEIGHT / 2);
+            transition.setToX((WIDTH / 2) + Math.cos(i * 2 * Math.PI / (NumOfPieces * 2) ));
+            transition.setToY((HEIGHT / 2) + Math.sin(i * 2 * Math.PI / (NumOfPieces * 2) ));
+            //transition.setByX(50);
+            //transition.setByY(25);
+            transition.setCycleCount(1);
+
+            transition.play();
+        }
     }
 
     public static void main(String[] args) {
