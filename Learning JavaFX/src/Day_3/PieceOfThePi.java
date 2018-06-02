0a7049a8cc95ecda194e5cd0111ae62d74574fc8
Vincent Lomino <vinlomino@gmail.com>
2018-06-01 22:59:01 -0400

    Boom, I am a god. I got PieceOfThePi to be animated. Javafx is very nice... once you understand how it works lol ;)

--- Learning JavaFX/src/Day_3/PieceOfThePi.java
+++ Learning JavaFX/src/Day_3/PieceOfThePi.java
@@ -1,5 +1,7 @@
 package Day_3;
 
+import javafx.animation.Animation;
+import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -7,11 +9,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
-//https://examples.javacodegeeks.com/desktop-java/javafx/javafx-animation-example/
 
 public class PieceOfThePi extends Application {
     @Override
@@ -21,9 +20,6 @@ public class PieceOfThePi extends Application {
         double RADIUS = 100;
         int NumOfPieces = 8;
         Pane pane = new Pane();
-        Circle testingCircle = new Circle(WIDTH / 2, HEIGHT / 2, RADIUS, Color.TRANSPARENT);
-        testingCircle.setStrokeWidth(1);
-        testingCircle.setStroke(Color.TRANSPARENT);
         Arc[] piePieces = new Arc[NumOfPieces];
         for (int i = 0; i < piePieces.length; i++) {
             piePieces[i] = new Arc();
@@ -39,39 +35,32 @@ public class PieceOfThePi extends Application {
             piePieces[i].setFill(Color.hsb(360 * i / NumOfPieces, 1.0, 1.0));
             pane.getChildren().add(piePieces[i]);
         }
-        /*Arc piece = new Arc();
-        piece.setCenterX((WIDTH / 2) + 20 * Math.cos(2 * Math.PI / (2 * NumOfPieces)));
-        piece.setCenterY((HEIGHT / 2) + 20 * Math.sin(2 * Math.PI / (2 * NumOfPieces)));
-        piece.setRadiusY(RADIUS);
-        piece.setRadiusX(RADIUS);
-        piece.setType(ArcType.ROUND);
-        piece.setLength(360 / NumOfPieces);
-        piece.setStartAngle(360 - (360 / NumOfPieces));
-        piece.setStrokeWidth(1);
-        piece.setStroke(Color.BLACK);
-        piece.setFill(Color.hsb(360 * (piePieces.length - 1) / NumOfPieces, 1.0, 1.0));
-        pane.getChildren().add(piece);*/
 
-        pane.getChildren().add(testingCircle);
         Scene scene = new Scene(pane, WIDTH, HEIGHT);
         stage.setTitle("Vincent Lomino");
         stage.setScene(scene);
         stage.show();
 
-        TranslateTransition transition = new TranslateTransition(Duration.millis(3000));
-
-        for (int i = 0; i < piePieces.length; i++) {
+        TranslateTransition[] transitions = new TranslateTransition[piePieces.length];
+        TranslateTransition transition;
+        for (int i = 0; i < transitions.length; i++) {
+            transition = new TranslateTransition(Duration.millis(500));
             transition.setNode(piePieces[i]);
-            transition.setFromX(WIDTH / 2);
-            transition.setFromY(HEIGHT / 2);
-            transition.setToX((WIDTH / 2) + Math.cos(i * 2 * Math.PI / (NumOfPieces * 2) ));
-            transition.setToY((HEIGHT / 2) + Math.sin(i * 2 * Math.PI / (NumOfPieces * 2) ));
-            //transition.setByX(50);
-            //transition.setByY(25);
-            transition.setCycleCount(1);
-
-            transition.play();
+            transition.setFromX(0);
+            transition.setFromY(0);
+            transition.setByX(50 * Math.sin((1 - Math.pow(NumOfPieces, -1)) * Math.PI + (2 * Math.PI * (i - 1) / NumOfPieces)));
+            transition.setByY(50 * Math.cos((1 - Math.pow(NumOfPieces, -1)) * Math.PI + (2 * Math.PI * (i - 1) / NumOfPieces)));
+            transition.setCycleCount(2);
+            transition.setAutoReverse(true);
+            transitions[i] = transition;
         }
+
+        SequentialTransition sequentialTransition = new SequentialTransition();
+        sequentialTransition.getChildren().addAll(transitions);
+        sequentialTransition.setCycleCount(Animation.INDEFINITE);
+        sequentialTransition.setAutoReverse(false);
+        sequentialTransition.play();
+
     }
 
     public static void main(String[] args) {
