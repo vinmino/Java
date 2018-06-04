63904b48b98b387e06087a342b2b0836356e0f37
Vincent Lomino <vinlomino@gmail.com>
2018-06-04 00:06:59 -0400

    I am trying to get RotateByThirteen to be a cryptography tool. So far it kinda works lol

--- Word Scrambler/src/thirteen/RotateByThirteen.java
+++ Word Scrambler/src/thirteen/RotateByThirteen.java
@@ -23,23 +23,23 @@ public class RotateByThirteen {
 	}
 
 
-	public static String thirteen(String user) {
+	public static String thirteen(String user, int shift, int direction) {
 		char curr;
-		char[] userArr = new char[user.length()];//Makes an array of char to hold the chars in the string
+		char[] userArr;//Makes an array of char to hold the chars in the string
 		int diff;
 		userArr = user.toCharArray(); //fills the array with the input string
-		for (int i = 0; i < user.length(); i++) { //iterates through the array and changes each one by 13
+		for (int i = 0; i < user.length(); i++) { //iterates through the array and changes each one by the shift * direction
 			curr = userArr[i];
 			if (btw_AZ(curr) || btw_az(curr)) { //checks to see if its a letter
-				curr += 13;
+				curr += shift * direction;
 			}
-			if (btw_AZ(curr) && btw_AZ((char)(curr - 13)) || btw_az(curr) && btw_az((char)(curr - 13)) ) { //checks if the letter casing changed. if its the same, then it doesn't need to be touched
+			if (btw_AZ(curr) && btw_AZ((char)(curr - shift * direction)) || btw_az(curr) && btw_az((char)(curr - shift * direction)) ) { //checks if the letter casing changed. if its the same, then it doesn't need to be touched
 				userArr[i] = curr;
 				continue;
-			} else if (btw_AZ((char)(curr - 13))) {//if it was uppercase now its not, it loops the letter back to the beginning
+			} else if (btw_AZ((char)(curr - shift * direction))) {//if it was uppercase now its not, it loops the letter back to the beginning
 				diff = curr - 'Z';
 				curr = (char)('A' - 1 + diff);
-			} else if (btw_az((char)(curr - 13))) {//if it was lowercase now its not, it loops the letter back to the beginning
+			} else if (btw_az((char)(curr - shift * direction))) {//if it was lowercase now its not, it loops the letter back to the beginning
 				diff = curr - 'z';
 				curr = (char)('a' - 1 + diff);
 			}
@@ -54,11 +54,25 @@ public class RotateByThirteen {
 	
 	public static void main(String[] args) {
 		Scanner key = new Scanner(System.in);
-		System.out.print("Input the string of text: ");
-		String user = key.nextLine();
-		
-		String result = thirteen(user);
-		System.out.println(result);
+		System.out.print("Cypher Type:\n\tEncryption: 1\n\tDecryption: 2\nChoice: ");
+		int choice = key.nextInt();
+		String user;
+		Scanner key2 = new Scanner(System.in);
+		if (choice == 2) {
+			System.out.print("Input the string of text: ");
+			user = key2.nextLine();
+			for (int i = 0; i < 26; i++) {
+				System.out.println(i + ":\t" + thirteen(user, i, 1));
+			}
+		} else if (choice == 1) {
+			System.out.print("Input the string of text: ");
+			user = key2.nextLine();
+			for (int i = 0; i < 26; i++) {
+				System.out.println(i + ":\t" + thirteen(user,  i, -1));
+			}
+		} else {
+			System.out.println("Come on dude. Follow the directions!");
+		}
 		key.close();
 		
 		
