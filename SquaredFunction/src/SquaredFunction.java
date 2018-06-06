d6efc03b62f881ac3ef4766639fd1f3fededd672
Vincent Lomino <vinlomino@gmail.com>
2018-06-06 16:41:48 -0400

    SquaredFunction now has X and Y labels for the axises

--- SquaredFunction/src/SquaredFunction.java
+++ SquaredFunction/src/SquaredFunction.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polyline;
@@ -10,27 +11,33 @@ public class SquaredFunction extends Application {
 
     public static double f(double x) {
         return -0.0125 * Math.pow(x, 2);
+        //return 100 * Math.sin(x/25);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        double WIDTH = 250;
-        double HEIGHT = 250;
+        double WIDTH = 500;
+        double HEIGHT = 500;
         double xCenter = WIDTH / 2;
         double yCenter = HEIGHT / 2;
         Pane root = new Pane();
         Line xAxis = new Line(xCenter, 0, xCenter, HEIGHT);
+        Label xLabel = new Label("X");
+        xLabel.translateYProperty().setValue(yCenter + 2);
+        xLabel.translateXProperty().setValue(2);
         Line yAxis = new Line(0, yCenter, WIDTH, yCenter);
-        double[] points = new double[(int) WIDTH * 2];
+        Label yLabel = new Label("Y");
+        yLabel.translateXProperty().setValue(xCenter + 2);
+        double[] points = new double[(int)WIDTH];
         for (double d = 0; d < WIDTH; d += 2) {
             points[(int)d] = d;
-            points[(int)d + 1] = f((d - xCenter)) + yCenter;
+            points[(int)d + 1] = f(d - xCenter) + yCenter;
         }
 
-        Polyline curve = new Polyline(points);
+        Polyline graph = new Polyline(points);
 
 
-        root.getChildren().addAll(xAxis, yAxis, curve);
+        root.getChildren().addAll(xAxis, yAxis, graph, xLabel, yLabel);
 
 
         Scene scene = new Scene(root, WIDTH, HEIGHT);
