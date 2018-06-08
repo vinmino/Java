4fc017dca5385d1403f1fe496da086f1e407084e
vinmino <vinlomino@gmail.com>
2018-06-08 11:03:18 -0400

    Tied to make it smiled

--- SmileyFace/src/SmileyFace.java
+++ SmileyFace/src/SmileyFace.java
@@ -1,6 +1,11 @@
+import javafx.animation.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+import javafx.util.Duration;
 
 public class SmileyFace extends Pane {
 
@@ -18,10 +23,44 @@ public class SmileyFace extends Pane {
         circle.setFill(Color.YELLOW);
         circle.setStroke(Color.BLACK);
 
+        Ellipse eyeLeft = new Ellipse(centerX - (faceRadius * 0.3), centerY - (faceRadius * 0.45), faceRadius * 0.05, faceRadius * 0.1);
+        eyeLeft.setFill(Color.BLACK);
+        Ellipse eyeRight = new Ellipse(centerX + (faceRadius * 0.3), centerY - (faceRadius * 0.45), faceRadius * 0.05, faceRadius * 0.1);
+        eyeLeft.setFill(Color.BLACK);
 
+        Arc smile = new Arc();
+        smile.setCenterX(centerX);
+        smile.setCenterY(centerY);
+        smile.setStartAngle(190);
+        smile.setLength(160);
+        smile.setType(ArcType.OPEN);
+        smile.setFill(Color.TRANSPARENT);
+        smile.setStrokeWidth(2);
+        smile.setStroke(Color.BLACK);
+        smile.setRadiusX(faceRadius * 0.6);
+        smile.setRadiusY(faceRadius * 0.4);
+
+        Transition radiusTranstion = new Transition()
+        {
+            {
+                setCycleDuration(Duration.millis(1000));
+                setAutoReverse(true);
+                setInterpolator(Interpolator.LINEAR);
+                setCycleCount(Animation.INDEFINITE);
+            }
+
+            @Override
+            protected void interpolate(double frac)
+            {
+                smile.setRadiusY((faceRadius * 0.4) - (faceRadius * 0.4 * frac));
+                smile.translateYProperty().setValue(faceRadius * 0.4 * frac);
+            }
+        };
+
+        radiusTranstion.play();
 
         getChildren().clear();
-        getChildren().addAll(circle);
+        getChildren().addAll(circle, eyeLeft, eyeRight, smile);
 
     }
 
