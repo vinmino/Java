01905f0d8f043a1dfd6d696c01264f52eb235812
vinmino <vinlomino@gmail.com>
2018-06-11 13:15:21 -0400

    Project Euler Problem 3

--- Project Euler/src/Problem_3/Largest_Prime_Factor.java
+++ Project Euler/src/Problem_3/Largest_Prime_Factor.java
@@ -4,11 +4,12 @@ public class Largest_Prime_Factor {
 
     public static void main(String[] args) {
         double num = 600851475143d;
-        for (double i = 2d; i*i <= num; i++) {
-            if (num % i == 0) {
-                System.out.println(i);
+        for (double i = 2; i <= num; i++) {
+            if (num%i == 0) {
+                while( num%i == 0)
+                    num = num/i;
+                System.out.println(i + "PRIME FACTOR");
             }
         }
     }
-
 }
